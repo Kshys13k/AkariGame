6d98617bfa7eb596f31ef53fc57040a4a00682b0
ignacy <01168951@pw.edu.pl>
2022-02-01 00:52:06 +0100

    final version

---
+++ src/akari/Main.java
@@ -0,0 +1,11 @@
+package akari;
+
+import akari.GUI.MainFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
